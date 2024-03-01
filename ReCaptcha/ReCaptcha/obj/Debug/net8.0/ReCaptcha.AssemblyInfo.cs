c89263b6ac38ac81821044c91a1b24ebf7533537
yeeydu <yeeysonduarte@gmail.com>
2024-03-01 20:38:13 +0000

    test2

--- ReCaptcha/ReCaptcha/obj/Debug/net8.0/ReCaptcha.AssemblyInfo.cs
+++ ReCaptcha/ReCaptcha/obj/Debug/net8.0/ReCaptcha.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReCaptcha")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41670beec7282f4a57f60b0de89464a0ff180dc1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24f30016aadb4af917acde2dd1b30b6869c29465")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReCaptcha")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReCaptcha")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
