8c07aab98d915484128e6113aab036dec6427564
yeeydu <yeeysonduarte@gmail.com>
2024-03-01 19:48:50 +0000

    anticaptcha project

---
+++ ReCaptcha/ReCaptcha/Key.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReCaptcha
+{
+    public class Key
+    {
+       // there most be a api.key token
+
+       "API_KEY" : "6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe"
+        
+    }
+}
---
+++ ReCaptcha/ReCaptcha/Program.cs
@@ -0,0 +1,17 @@
+﻿using EasyAutomationFramework;
+using TwoCaptcha;
+
+const string link = "https://www.google.com/recaptcha/api2/demo";
+
+var web = new Web();
+web.StartBrowser();
+web.Navigate(link);
+web.WaitForLoad();
+
+var catpcha_key = web.GetValue(TypeElement.Id, "recaptcha-demo").element.GetAttribute("data-sitekey");
+
+var solver = new Solve();
+var result = await solver.ReCaptchaV2Async(twoCaptchaKey: ReCaptcha.Key.API_KEY, catpcha_key, link);
+
+web.ExecuteScript($"document.querySelector('#g-recaptcha-response').innerHTML = '{result.Request}';");
+web.Click(TypeElement.Id, "recaptcha-demo-submit");
